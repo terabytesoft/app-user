5e6617a9896f406eabd446a72ef251b5d7d57b38
terabytesoftw <terabytesoftw@gmail.com>
2019-01-21 10:45:41 -0400

    Update composer.json

--- src/views/admin/index.php
+++ src/views/admin/index.php
@@ -8,16 +8,14 @@ use yii\web\View;
 
 /**
  * @var \yii\web\View $this
- * @var \yii\data\ActiveDataProvider $dataProvider
- * @var \dektrium\user\models\UserSearch $searchModel
+ * @var \yii\activerecord\data\ActiveDataProvider $dataProvider
+ * @var \app\user\models\UserSearch $searchModel
  */
 
-$this->title = $this->getApp()->t('user', 'Manage users');
+$this->title = $this->app->t('user', 'Manage users');
 $this->params['breadcrumbs'][] = $this->title;
 
 ?>
-
-<?= $this->render('/_alert', ['module' => $this->getApp()->getModule('user')]) ?>
 
 <?= $this->render('/admin/_menu') ?>
 
@@ -36,7 +34,7 @@ $this->params['breadcrumbs'][] = $this->title;
 				'attribute' => 'registration_ip',
 				'value' => function ($model) {
 					return $model->registration_ip == null
-						? '<span class="not-set">' . $this->getApp()->t('user', '(not set)') . '</span>'
+						? '<span class="not-set">' . $this->app->t('user', '(not set)') . '</span>'
 						: $model->registration_ip;
 				},
 			],
@@ -44,7 +42,7 @@ $this->params['breadcrumbs'][] = $this->title;
 				'attribute' => 'created_at',
 				'value' => function ($model) {
 					if (extension_loaded('intl')) {
-						return $this->getApp()->t('user', '{0, date, MMMM dd, YYYY HH:mm}', [$model->created_at]);
+						return $this->app->t('user', '{0, date, MMMM dd, YYYY HH:mm}', [$model->created_at]);
 					} else {
 						return date('Y-m-d G:i:s', $model->created_at);
 					}
@@ -54,46 +52,46 @@ $this->params['breadcrumbs'][] = $this->title;
 				'attribute' => 'last_login_at',
 				'value' => function ($model) {
 					if (!$model->last_login_at || $model->last_login_at == 0) {
-						return $this->getApp()->t('user', 'Never');
+						return $this->app->t('user', 'Never');
 				  	} else if (extension_loaded('intl')) {
-						return $this->getApp()->t('user', '{0, date, MMMM dd, YYYY HH:mm}', [$model->last_login_at]);
+						return $this->app->t('user', '{0, date, MMMM dd, YYYY HH:mm}', [$model->last_login_at]);
 				  	} else {
 						return date('Y-m-d G:i:s', $model->last_login_at);
 				  	}
 				},
 			],
 			[
-				'header' => $this->getApp()->t('user', 'Confirmation'),
+				'header' => $this->app->t('user', 'Confirmation'),
 				'value' => function ($model) {
 					if ($model->isConfirmed) {
 						return '<div class="text-center">
-									<span class="text-success">' . $this->getApp()->t('user', 'Confirmed') . '</span>
+									<span class="text-success">' . $this->app->t('user', 'Confirmed') . '</span>
 								</div>';
 					} else {
-						return Html::a($this->getApp()->t('user', 'Confirm'), ['confirm', 'id' => $model->id], [
+						return Html::a($this->app->t('user', 'Confirm'), ['confirm', 'id' => $model->id], [
 							'class' => 'btn btn-xs btn-success btn-block',
 							'data-method' => 'post',
-							'data-confirm' => $this->getApp()->t('user', 'Are you sure you want to confirm this user?'),
+							'data-confirm' => $this->app->t('user', 'Are you sure you want to confirm this user?'),
 						]);
 					}
 				},
 				'format' => 'raw',
-				'visible' => $this->getApp()->getModule('user')->enableConfirmation,
+				'visible' => $this->app->getModule('user')->enableConfirmation,
 			],
 			[
-				'header' => $this->getApp()->t('user', 'Block status'),
+				'header' => $this->app->t('user', 'Block status'),
 				'value' => function ($model) {
 					if ($model->isBlocked) {
-						return Html::a($this->getApp()->t('user', 'Unblock'), ['block', 'id' => $model->id], [
+						return Html::a($this->app->t('user', 'Unblock'), ['block', 'id' => $model->id], [
 							'class' => 'btn btn-xs btn-success btn-block',
 							'data-method' => 'post',
-							'data-confirm' => $this->getApp()->t('user', 'Are you sure you want to unblock this user?'),
+							'data-confirm' => $this->app->t('user', 'Are you sure you want to unblock this user?'),
 						]);
 					} else {
-						return Html::a($this->getApp()->t('user', 'Block'), ['block', 'id' => $model->id], [
+						return Html::a($this->app->t('user', 'Block'), ['block', 'id' => $model->id], [
 							'class' => 'btn btn-xs btn-danger btn-block',
 							'data-method' => 'post',
-							'data-confirm' => $this->getApp()->t('user', 'Are you sure you want to block this user?'),
+							'data-confirm' => $this->app->t('user', 'Are you sure you want to block this user?'),
 						]);
 					}
 				},
@@ -106,16 +104,16 @@ $this->params['breadcrumbs'][] = $this->title;
 					'resend_password' => function ($url, $model, $key) {
 						if ($model->isAdmin) {
 							return '
-						<a data-method="POST" data-confirm="' . $this->getApp()->t('user', 'Are you sure?') . '" href="' . Url::to(['resend-password', 'id' => $model->id]) . '">
-						<span title="' . $this->getApp()->t('user', 'Generate and send new password to user') . '" class= "&#x2709">
+						<a data-method="POST" data-confirm="' . $this->app->t('user', 'Are you sure?') . '" href="' . Url::to(['resend-password', 'id' => $model->id]) . '">
+						<span title="' . $this->app->t('user', 'Generate and send new password to user') . '" class= "&#x2709">
 						</span> </a>';
 						}
 					},
 					'switch' => function ($url, $model) {
-						if ($model->id != $this->getApp()->user->id && $this->getApp()->getModule('user')->enableImpersonateUser) {
+						if ($model->id != $this->app->user->id && $this->app->getModule('user')->enableImpersonateUser) {
 							return Html::a('<span class="glyphicon glyphicon-user"></span>', ['/user/admin/switch', 'id' => $model->id], [
-								'title' => $this->getApp()->t('user', 'Become this user'),
-								'data-confirm' => $this->getApp()->t('user', 'Are you sure you want to switch to this user for the rest of this Session?'),
+								'title' => $this->app->t('user', 'Become this user'),
+								'data-confirm' => $this->app->t('user', 'Are you sure you want to switch to this user for the rest of this Session?'),
 								'data-method' => 'POST',
 							]);
 						}
@@ -124,3 +122,4 @@ $this->params['breadcrumbs'][] = $this->title;
 			],
 		],
 ]);
+
