d2dfa7cf05e02d73565317409b6420ff7afcb9b5
terabytesoftw <terabytesoftw@gmail.com>
2019-05-09 12:36:45 -0400

    Fix test codeception

--- tests/_support/Helper/Acceptance.php
+++ tests/_support/Helper/Acceptance.php
@@ -7,21 +7,4 @@ namespace Helper;
 
 class Acceptance extends \Codeception\Module
 {
-   /**
-     * @param string $old
-     * @param string $new
-     **/
-    public static function paramUpdate(string $old, string $new)
-    {
-        $path = __DIR__ . '/../../../config/params.php';
-
-        if (file_exists($path)) {
-            file_put_contents($path, str_replace(
-                $old,
-                $new,
-                file_get_contents($path)
-            ));
-            echo exec('composer du');
-        }
-    }
 }
